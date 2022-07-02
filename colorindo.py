b49db5ac1c81687cded58ca4b0ad409a66ce98ba
Dione Rigamonti <dionerigamonti@hotmail.com>
2022-07-02 11:32:41 -0300

    colorindo.py
    Testando a biblioteca colorama.

---
+++ colorindo.py
@@ -0,0 +1,12 @@
+from colorama import Back, Fore, init
+# - - Biblioteca destinada para a coloração do terminal.
+init(convert=True , autoreset=True)
+
+print(Fore.GREEN + 'Hello World')
+print(Back.GREEN + 'Hello World')
+print(Fore.YELLOW + 'Hello World')
+print(Back.YELLOW + 'Hello World')
+print(Fore.BLUE + 'Hello World')
+print(Back.BLUE + 'Hello World')
+print(Fore.RED + 'Hello World')
+print(Back.RED + 'Hello World')
